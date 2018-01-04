351ca50a142de12384a045a19605e1e30b557045
ReminoR <victor.shamakov@gmail.com>
2018-01-04 23:10:20 +0700

    Added the field 'date'

--- lab3.py
+++ lab3.py
@@ -8,13 +8,15 @@ soup = BeautifulSoup(site, 'html.parser')
 
 title = soup.select('#new .post_list .post_list_item_title span a')
 autor = soup.select('#new .post_list .post_list_item_autor a')
+date = soup.select('#new .post_list .post_list_item_date')
 desc = soup.select('#new .post_list .post_list_item_text')
 
-with open('info.html', 'w') as file:
+with open('info.html', 'a') as file:
 	for i in range(len(title)):
 		file.write('<div class="block">')
 		file.write('<strong>Заголовок: </strong>' + title[i].get_text() + '<br>')
 		file.write('<strong>Автор: </strong>' + autor[i].get_text() + '<br>')
+		file.write('<strong>Дата: </strong>' + date[i].get_text() + '<br>')
 		file.write('<strong>Описание: </strong>' + desc[i].get_text() + '<br>')
 		file.write('</div><br>')
 	file.close()
